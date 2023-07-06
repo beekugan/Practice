8fc2577b8f5baa52c91f84d7141bdfec91564a89
beekugan <138242236+beekugan@users.noreply.github.com>
2023-07-06 19:09:29 +0300

    Update Task 5.c

--- Task 5.c
+++ Task 5.c
@@ -23,14 +23,19 @@ int main() {
     int n;
 
     // Request to enter the length of the sequence
-    printf("Enter the length of the sequencen: ");
+    printf("Enter the length of the sequence: ");
     scanf("%d", &n);
 
-    // Call the countSequences function to count the number of sequences
-    int result = countSequences(n);
+    // Check if n is within the specified range
+    if (n > 1 && n < 10000) {
+        // Call the countSequences function to count the number of sequences
+        int result = countSequences(n);
 
-    // Outputting the result
-    printf("Number of searched sequences: %d\n", result);
+        // Outputting the result
+        printf("Number of searched sequences: %d\n", result);
+    } else {
+        printf("Invalid input. Length of the sequence should be between 1 and 9999.\n");
+    }
 
     return 0;
 }
